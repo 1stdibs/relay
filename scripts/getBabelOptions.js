37cb9305519e3f310afc92efd1b152d14a194b89
Rob Richard <rob@1stdibs.com>
2017-09-18 10:33:31 -0400

    do not inline requires
    # Conflicts:
    #	scripts/getBabelOptions.js

--- scripts/getBabelOptions.js
+++ scripts/getBabelOptions.js
@@ -22,7 +22,7 @@ module.exports = function(options) {
   const fbjsPreset = require('babel-preset-fbjs/configure')({
     autoImport: options.autoImport || false,
     objectAssign: false,
-    inlineRequires: true,
+    inlineRequires: false,
     stripDEV: options.env === 'production',
   });
 
